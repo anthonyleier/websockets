0cf9e793b01533928f120b65ee9192b3b40df276
Anthony Cruz <anthonyleierlw@gmail.com>
2023-12-14 14:15:24 -0300

    Criação da classe de cliente

--- client.py
+++ client.py
@@ -1,34 +1,52 @@
 import socket
 import threading
 
-nickname = input('Choose your nickname: ')
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 7976))
+class Client:
+    def __init__(self):
+        self.conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ip = 'localhost'
+        self.porta = 7976
+        self.nickname = None
 
+    def cadastrar(self, nickname):
+        self.nickname = nickname
+        self.conexao.send(nickname.encode('ASCII'))
 
-def receive():
-    while True:
-        try:
-            message = client.recv(1024).decode('ASCII')
-            if message == 'NICKNAME':
-                client.send(nickname.encode('ASCII'))
-            else:
+    def iniciar_chat(self):
+        recebimento_thread = threading.Thread(target=self.recebimento)
+        recebimento_thread.start()
+        envio_thread = threading.Thread(target=self.envio)
+        envio_thread.start()
+
+    def recebimento(self):
+        while True:
+            try:
+                message = self.conexao.recv(1024).decode('ASCII')
                 print(message)
-        except:
-            print('Deu ruim mermao')
-            client.close()
-            break
+            except:
+                print('Deu ruim mermao')
+                self.conexao.close()
+                break
+
+    def envio(self):
+        while True:
+            message = '{}: {}'.format(self.nickname, input(''))
+            self.conexao.send(message.encode('ASCII'))
+
+    def conectar(self):
+        self.conexao.connect((self.ip, self.porta))
+
+    def desconectar(self):
+        self.conexao.close()
 
 
-def write():
-    while True:
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('ASCII'))
+def main():
+    cliente = Client()
+    cliente.conectar()
+    cliente.cadastrar(input("Qual o seu nome?"))
+    cliente.iniciar_chat()
 
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
-
-write_thread = threading.Thread(target=write)
-write_thread.start()
+if __name__ == "__main__":
+    main()
