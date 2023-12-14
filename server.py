2dc1bc501f5c20bf15dda25c3f4d8241f16380ad
Anthony Cruz <anthonyleierlw@gmail.com>
2023-12-14 08:58:36 -0300

    Refatoração de servidor

--- server.py
+++ server.py
@@ -2,16 +2,27 @@ import asyncio
 import websockets
 
 
-async def rodar_servidor(websocket, caminho):
-    mensagens = []
+class Server:
+    def __init__(self, websocket):
+        self.websocket = websocket
+        self.mensagens = []
 
-    while True:
-        mensagem_recebida = await websocket.recv()
-        mensagens.append(mensagem_recebida)
-
-        mensagens_texto = '\n'.join(mensagens)
+    async def enviar_mensagem(self):
+        mensagens_texto = '\n'.join(self.mensagens)
         print(mensagens_texto)
-        await websocket.send(mensagens_texto)
+        await self.websocket.send(mensagens_texto)
+
+    async def receber_mensagem(self):
+        mensagem_recebida = await self.websocket.recv()
+        self.mensagens.append(mensagem_recebida)
+
+
+async def rodar_servidor(websocket):
+    server = Server(websocket)
+    while True:
+        await server.receber_mensagem()
+        await server.enviar_mensagem()
+
 
 asyncio.get_event_loop().run_until_complete(websockets.serve(rodar_servidor, 'localhost', 8765))
 asyncio.get_event_loop().run_forever()
