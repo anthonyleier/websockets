18da167c65cd9a7ddb67428fd10de73de5e648b7
Anthony Cruz <anthonyleierlw@gmail.com>
2023-12-14 10:18:09 -0300

    fuck

--- client.py
+++ client.py
@@ -1,40 +1,33 @@
+import os
 import asyncio
 import websockets
-from websockets.exceptions import ConnectionClosedOK
 
 
-class Client:
-    def __init__(self, websocket):
-        self.websocket = websocket
-
-    async def enviar_mensagem(self, texto):
-        await self.websocket.send(texto)
-
-    async def receber_mensagem(self):
-        try:
-            resposta = await self.websocket.recv()
-            print(resposta)
-        except ConnectionClosedOK:
-            print("Conexão fechada pelo servidor")
-
-
-async def rodar_cliente():
+async def enviar_mensagens():
     uri = "ws://localhost:8765"
 
     async with websockets.connect(uri) as websocket:
-        client = Client(websocket)
+        websocket.send(input('identi'))
 
-        identificacao = input('Identificação:')
-        await client.enviar_mensagem(identificacao)
-        await client.receber_mensagem()
+
+async def receber_mensagens():
+    uri = "ws://localhost:8765"
+    historico = ''
+
+    async with websockets.connect(uri) as websocket:
+
+        with open('texto.txt', 'w', encoding='UTF8') as arquivo:
+            arquivo.write('start\n')
 
         while True:
-            destinatario = input("Digite o identificador do destinatario:")
-            conteudo = input("Digite a mensagem:")
-            mensagem = f"{destinatario}:{conteudo}"
+            resposta = await websocket.recv()
 
-            await client.enviar_mensagem(mensagem)
-            await client.receber_mensagem()
+            if resposta != historico:
+                historico = resposta
+                with open('texto.txt', 'a', encoding='UTF8') as arquivo:
+                    arquivo.write(historico)
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(rodar_cliente())
+    asyncio.gather(enviar_mensagens(), receber_mensagens())
+    asyncio.get_event_loop().run_forever()
+    # asyncio.get_event_loop().run_until_complete(rodar_cliente())
--- server.py
+++ server.py
@@ -1,43 +1,24 @@
 import asyncio
 import websockets
 
-
-class Server:
-    def __init__(self, websocket):
-        self.websocket = websocket
-        self.clients = []
-
-    async def registar_client(self):
-        identificacao = await self.websocket.recv()
-        self.clients[identificacao] = self.websocket
-
-    async def enviar_mensagem(self):
-        mensagens_texto = '\n'.join(self.mensagens)
-        print(mensagens_texto)
-        await self.websocket.send(mensagens_texto)
-
-    async def receber_mensagem(self):
-        mensagem_recebida = await self.websocket.recv()
-        self.mensagens.append(mensagem_recebida)
+mensagens = []
 
 
-clientes = {}
+def gerar_mensagens_texto():
+    texto = ''
+    for mensagem in mensagens:
+        texto += f"{mensagem['user']} - {mensagem['message']}\n"
+    return texto
 
 
 async def rodar_servidor(websocket):
-    identificacao = await websocket.recv()
-    await websocket.send(f'Identificado como {identificacao}')
-    clientes[identificacao] = websocket
+    mensagens.append({'user': 'server', 'message': 'opa'})
 
-    async for mensagem in websocket:
-        destinatario, conteudo = mensagem.split(":")
-        destinatario_socket = clientes.get(destinatario)
-
-        if not destinatario_socket:
-            await websocket.send("Destinatario não encontrado")
-
-        if destinatario_socket:
-            await destinatario_socket.send(f"Mensagem de outra pessoa: {conteudo}")
+    while True:
+        print(await websocket.recv())
+        mensagens_texto = gerar_mensagens_texto()
+        print(mensagens_texto)
+        await websocket.send(mensagens_texto)
 
 
 asyncio.get_event_loop().run_until_complete(websockets.serve(rodar_servidor, 'localhost', 8765))
