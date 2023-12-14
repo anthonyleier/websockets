0583a918ad8b989ca3542e97ade4ead2ee5fc82d
Anthony Cruz <anthonyleierlw@gmail.com>
2023-12-14 14:44:54 -0300

    Tradução da classe de client

--- client.py
+++ client.py
@@ -3,54 +3,56 @@ import threading
 
 
 class Client:
-    def __init__(self):
-        self.conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = 'localhost'
-        self.porta = 7976
+    def __init__(self, host, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.port = port
         self.nickname = None
 
-    def conectar(self):
-        self.conexao.connect((self.host, self.porta))
+    def connect(self):
+        self.connection.connect((self.host, self.porta))
 
-    def desconectar(self):
-        self.conexao.close()
+    def disconnect(self):
+        self.connection.close()
 
-    def cadastrar(self, nickname):
+    def register(self, nickname):
         self.nickname = nickname
-        self.conexao.send(nickname.encode('UTF8'))
+        self.connection.send(nickname.encode('UTF8'))
 
-    def iniciar_chat(self):
-        recebimento_thread = threading.Thread(target=self.recebimento)
-        recebimento_thread.start()
-        envio_thread = threading.Thread(target=self.envio)
-        envio_thread.start()
+    def start_chat(self):
+        receive_thread = threading.Thread(target=self.receive)
+        receive_thread.start()
+        send_thread = threading.Thread(target=self.send)
+        send_thread.start()
 
-    def recebimento(self):
+    def receive(self):
         while True:
             try:
-                message = self.conexao.recv(1024).decode('UTF8')
+                message = self.connection.recv(1024).decode('UTF8')
                 print(message)
+
             except Exception as erro:
                 print(f'Ocorreu um erro no recebimento das mensagens: {erro}')
-                self.desconectar()
+                self.disconnect()
                 break
 
-    def envio(self):
+    def send(self):
         while True:
             try:
                 message = '{}: {}'.format(self.nickname, input(''))
-                self.conexao.send(message.encode('UTF8'))
+                self.connection.send(message.encode('UTF8'))
+
             except Exception as erro:
                 print(f'Ocorreu um erro no envio das mensagens: {erro}')
-                self.desconectar()
+                self.disconnect()
                 break
 
 
 def main():
-    client = Client()
-    client.conectar()
-    client.cadastrar(input("Nickname:"))
-    client.iniciar_chat()
+    client = Client('localhost', 7976)
+    client.connect()
+    client.register(input("Nickname:"))
+    client.start_chat()
 
 
 if __name__ == "__main__":
